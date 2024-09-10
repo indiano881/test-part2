b3d6d5533ee954ec0b5e6f0ceaeb016abbf5c3d6
Davide Baldi <d.baldi88@gmail.com>
2024-09-10 14:46:39 +0200

    optimized components

--- src/app/components/UserName/index.tsx
+++ src/app/components/UserName/index.tsx
@@ -1,21 +1,37 @@
-"use client"
+"use client";
 
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
-
-type UserNameProps= {
-    updateUser: () => {}
-}
-
-const UserName = ({updateUser}:UserNameProps) => {
- const [name, setName] = useState<string>("")
-    return (
-        <form>
-            <label htmlFor="name-input" >Enter your name below</label>
-            <input id="name-input" />
-          <button data-testid="add-userName">Save</button>
-        </form>
-    )
+type UserNameProps = {
+  updateUser: (name: string) => void;  
 };
 
-export default UserName
+const UserName = ({ updateUser }: UserNameProps) => {
+  const [name, setName] = useState<string>("");
+
+ 
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+ 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault(); 
+    updateUser(name);    
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name-input">Enter your name below</label>
+      <input
+        id="name-input"
+        value={name}
+        onChange={handleInput}
+        aria-label="Enter your name below"
+      />
+      <button type="submit" data-testid="add-userName">Save</button>
+    </form>
+  );
+};
+
+export default UserName;
