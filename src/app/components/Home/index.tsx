bfcf923cfd5510c66bd48265a8e8f920fcd6d7e5
Davide Baldi <d.baldi88@gmail.com>
2024-09-11 10:51:09 +0200

    only one test missing

--- src/app/components/Home/index.tsx
+++ src/app/components/Home/index.tsx
@@ -7,20 +7,35 @@ import DailyChallenge from "../DailyChallenge";
 import Stretching from "../Stretching";
 import Cardio from "../Cardio";
 import Gym from "../Gym";
+import DisplayCard from "../DisplayCard";
 
 //must understand components they go inside 
 //state that saves the name?
 const Home = () => {
 const [savedName,setSavedName]=useState<string>("")
+const [showPage, setShowPage] = useState<string>("")
 
     return (
         <div>
-            <UserName updateUser={setSavedName} />
-            <HeroMessage name={savedName} />
-            <Cardio updateFunction={undefined} /> 
-            <Gym updateFunction={undefined}/>
-            <DailyChallenge updateFunction={undefined}/>
-            <Stretching updateFunction={undefined}/>
+            
+            {savedName==="" &&
+                <UserName updateUser={setSavedName} />
+            }
+            {savedName!=="" && 
+                <>
+                    <HeroMessage name={savedName} />
+                    <Cardio updateFunction={undefined} /> 
+                    <Gym updateFunction={undefined}/>
+                    <DailyChallenge updateFunction={undefined}/>
+                    <Stretching updateFunction={undefined}/>
+                </> 
+            }
+            {showPage==="exercise" && 
+                <>
+                    <DisplayCard onClick={undefined} workout={undefined} type={""} />
+                </> 
+            }
+            
           
         </div>
     )
