76fc60dc6ba42d749be5a02da92651fc01229553
Davide Baldi <d.baldi88@gmail.com>
2024-09-11 09:24:32 +0200

    1 more test passed

--- src/app/components/DisplayCard/index.tsx
+++ src/app/components/DisplayCard/index.tsx
@@ -1,11 +1,19 @@
+type DisplayCardprops= {
+    onClick: any,
+    workout: any,
+    type: string
+}
 
 
 
-
-const DisplayCard = () => {
+const DisplayCard = ({onClick, workout, type}:DisplayCardprops) => {
     return (
         <div>
-          
+          <h3>Run</h3>
+          <p>3 x 800m</p>
+          <h3>Sprint Intervals</h3>
+          <p>10 x 100m</p>
+          <button>BACK</button>
         </div>
     )
 };
