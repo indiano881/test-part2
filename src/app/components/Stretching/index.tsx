667a932693f222c08d613e4e417712f47904fdac
Davide Baldi <d.baldi88@gmail.com>
2024-09-11 09:02:16 +0200

    2 more test passed

---
+++ src/app/components/Stretching/index.tsx
@@ -0,0 +1,14 @@
+type StretchingProps = {
+    updateFunction: any
+}
+
+const Stretching = ({updateFunction}:StretchingProps) => {
+    return (
+        <div>
+          <h2>Mobility</h2>
+          <button>Get workout</button>
+        </div>
+    )
+};
+
+export default Stretching
